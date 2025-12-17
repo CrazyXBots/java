5a06be4a0f25c18d2ec29736d10b926b5054a20a
CrazyXBots <147149895+CrazyXBots@users.noreply.github.com>
2025-12-17 19:01:26 +0530

    Add Home component for successful deployment message

---
+++ pages/541/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '100px'}}>
+      <h1>Deployment Successful!</h1>
+      <p>Go to your page: <a href="/548/551/play">/548/551/play</a></p>
+    </div>
+  );
+}
