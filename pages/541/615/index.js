e95834a2e7c2bc5fa36f21f48a44fe48680c2457
CrazyXBots <147149895+CrazyXBots@users.noreply.github.com>
2025-12-17 19:01:49 +0530

    Delete pages/541/615/index.js

--- pages/541/615/index.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Site is Online!</h1>
-      <p>Go to <a href="/548/551/play">/548/551/play</a> to see your script.</p>
-    </div>
-  );
-}
