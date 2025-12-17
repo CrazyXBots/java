205e49fb87d0ac7751c0c27f80dad1b11194e157
CrazyXBots <147149895+CrazyXBots@users.noreply.github.com>
2025-12-17 18:54:02 +0530

    Update index.js

--- pages/541/615/index.js
+++ pages/541/615/index.js
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Welcome to my Next.js Site!</h1>;
+export default function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Site is Online!</h1>
+      <p>Go to <a href="/548/551/play">/548/551/play</a> to see your script.</p>
+    </div>
+  );
 }
