f8a528b9ba9f0fa01b3284c524882c88634ac435
CrazyXBots <147149895+CrazyXBots@users.noreply.github.com>
2025-12-17 19:31:24 +0530

    Update play.js

--- pages/548/play.js
+++ pages/548/play.js
@@ -1,1 +1,20 @@
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+
+export default function PlayPage() {
+  useEffect(() => {
+    // Put your player logic here so it only runs in the browser
+    window.vlc_player = () => { /* logic */ };
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>Video Player</title>
+        <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
+      </Head>
+      <video id="myVideo" controls></video>
+    </div>
+  );
+}
 
